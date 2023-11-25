7d889548c9e394643713fc0528a83a70c6721bb5
SIgabor <79259040+SIgabor@users.noreply.github.com>
2023-11-25 20:43:13 +0100

    Search added

--- app/src/main/java/com/example/komplex/MainActivity3.java
+++ app/src/main/java/com/example/komplex/MainActivity3.java
@@ -13,15 +13,14 @@ import android.widget.Toast;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class MainActivity3 extends AppCompatActivity implements SelectListener{
 
     private TextView tv_bag;
+    private MyAdapter myAdapter;
+    private List<Item> items;
     private List<Item> bagItems;
-
 
 
     @Override
@@ -29,11 +28,31 @@ public class MainActivity3 extends AppCompatActivity implements SelectListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
+
+        SearchView searchView = findViewById(R.id.searchView);
+        Log.d("MyTag", "searchView found");
+        searchView.clearFocus();
+        Log.d("MyTag", "searchView focus cleared");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.d("MyTag", "onQueryTextChange opened");
+                filterList(newText);
+                return false;
+            }
+        });
+
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        bagItems = new ArrayList<Item>();
+        recyclerView.setAdapter(myAdapter);
+        bagItems = new ArrayList<>();
         tv_bag= findViewById(R.id.tv_bag);
 
-        List<Item> items = new ArrayList<Item>();
+        items = new ArrayList<>();
         items.add(new Item("Tej", 300, R.drawable.tej, false));
         items.add(new Item("Tej1", 300, R.drawable.tej, false));
         items.add(new Item("Tej2", 300, R.drawable.tej, false));
@@ -43,20 +62,37 @@ public class MainActivity3 extends AppCompatActivity implements SelectListener{
         items.add(new Item("Tej6", 300, R.drawable.tej, false));
         items.add(new Item("Tej7", 300, R.drawable.tej, false));
         items.add(new Item("Tej8", 300, R.drawable.tej, false));
-        items.add(new Item("Tej9", 300, R.drawable.tej, false));
-        items.add(new Item("Tej10", 300, R.drawable.tej, false));
+        items.add(new Item("Alma", 300, R.drawable.tej, false));
+        items.add(new Item("Korte", 300, R.drawable.tej, false));
         items.add(new Item("Tej11", 300, R.drawable.tej, false));
         items.add(new Item("Tej12", 300, R.drawable.tej, false));
-        items.add(new Item("Tej13", 300, R.drawable.tej, false));
+        items.add(new Item("Szilva", 300, R.drawable.tej, false));
         items.add(new Item("Tej14", 300, R.drawable.tej, false));
-        items.add(new Item("Tej15", 300, R.drawable.tej, false));
+        items.add(new Item("Kenyer", 300, R.drawable.tej, false));
 
 
-
+        myAdapter = new MyAdapter(getApplicationContext(), items, this);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new MyAdapter(getApplicationContext(), items, this));
+        recyclerView.setAdapter(myAdapter);
 
     }
+
+    private void filterList(String text) {
+        List<Item> filteredList = new ArrayList<>();
+        for(Item item : items){
+            if(item.getName().toLowerCase().contains(text.toLowerCase())){
+                filteredList.add(item);
+                Log.d("MyTag", "item added");
+            }
+        }
+
+        if(filteredList.isEmpty()){
+            Toast.makeText(this, "Nincs ilyen termék!", Toast.LENGTH_SHORT).show();
+        }else{
+            myAdapter.setFilteredList(filteredList);
+        }
+    }
+
 
     @Override
     public void onBackPressed(){
@@ -77,9 +113,9 @@ public class MainActivity3 extends AppCompatActivity implements SelectListener{
     public void onItemClicked(Item item) {
 
         item.setChecked(!item.getChecked());
-        if(item.getChecked() == true){
+        if(item.getChecked()){
             bagItems.add(item);
-        } else if (item.getChecked() == false) {
+        } else if (!item.getChecked()) {
             bagItems.remove(item);
         }
         tv_bag.setText(String.valueOf(bagItems.size()));
--- app/src/main/java/com/example/komplex/MyAdapter.java
+++ app/src/main/java/com/example/komplex/MyAdapter.java
@@ -2,13 +2,17 @@ package com.example.komplex;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -18,24 +22,29 @@ public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
     private Context context;
     private List<Item> items;
     private SelectListener listener;
-    private Color color;
+
 
     public MyAdapter(Context context, List<Item> items, SelectListener listener) {
         this.context = context;
         this.items = items;
         this.listener = listener;
     }
+    
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Collections.sort(items, new AbcComparator());
+        Log.d("MyTag", "onCreateViewHolder sorted");
         return new MyViewHolder(LayoutInflater.from(context).inflate(R.layout.each_item, parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
         Collections.sort(items, new AbcComparator());
         Collections.sort(items, new CustomComparator().reversed());
+        Log.d("MyTag", "onBindViewHolder");
         holder.nameView.setText(items.get(position).getName());
         holder.priceView.setText(items.get(position).getPrice());
         holder.imageview.setImageResource(items.get(position).getImage());
@@ -51,6 +60,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
             public void onClick(View view) {
                 listener.onItemClicked(items.get(holder.getAdapterPosition()));
 
+                Log.d("MyTag", "sorted");
                 notifyDataSetChanged();
             }
         });
@@ -63,6 +73,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
     public int getItemCount() {
         return items.size();
     }
+
 
     public class CustomComparator implements Comparator<Item> {
         @Override
@@ -77,6 +88,15 @@ public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
             return o1.getName().compareTo(o2.getName());
         }
     }
+
+    public void setFilteredList(List<Item> filteredList){
+        this.items = filteredList;
+        Log.d("MyTag", "setFilteredList items changed");
+        notifyDataSetChanged();
+        Log.d("MyTag", "adapter notified");
+    }
+
+
 
 
 }
