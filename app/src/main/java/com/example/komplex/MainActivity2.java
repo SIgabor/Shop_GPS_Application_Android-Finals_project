3ea8809da09eefbb43de7a6e94b25ac36a2862cb
SIgabor <79259040+SIgabor@users.noreply.github.com>
2023-11-26 15:52:38 +0100

    Revert "fail"
    This reverts commit d0d308f4a0557020526e9d7e8a4b7bde85c5b81f.

--- app/src/main/java/com/example/komplex/MainActivity2.java
+++ app/src/main/java/com/example/komplex/MainActivity2.java
@@ -1,42 +1,19 @@
 package com.example.komplex;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentSender;
-import android.location.Location;
-import android.location.LocationManager;
-import android.location.LocationRequest;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.ResolvableApiException;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResponse;
-import com.google.android.gms.location.LocationSettingsStatusCodes;
-import com.google.android.gms.location.Priority;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    private TextView textView, tv_address;
-    private String str;
-    private Button LocationButton = findViewById(R.id.btn_getLocation);
-    private com.google.android.gms.location.LocationRequest locationRequest;
+    TextView textView;
+    String str;
 
 
 
@@ -63,80 +40,9 @@ public class MainActivity2 extends AppCompatActivity {
         }
 
         textView.setText(str);
-        tv_address = findViewById(R.id.tv_address);
-
-        locationRequest =  LocationRequest.build();
-        locationRequest.setPriority(Priority.PRIORITY_HIGH_ACCURACY);
-        locationRequest.setInterval(5000);
-        locationRequest.setFastestInterval(2000);
-
-        LocationButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(isGPSEnabled()){
-
-                }else{
-                    turnOnGPS();
-                }
-
-
-            }
-        });
 
 
     }
-
-    private void turnOnGPS() {
-
-
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
-                .addLocationRequest(locationRequest);
-        builder.setAlwaysShow(true);
-
-        Task<LocationSettingsResponse> result = LocationServices.getSettingsClient(getApplicationContext())
-                .checkLocationSettings(builder.build());
-
-        result.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
-            @Override
-            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
-
-                try {
-                    LocationSettingsResponse response = task.getResult(ApiException.class);
-                    Toast.makeText(MainActivity2.this, "GPS is already tured on", Toast.LENGTH_SHORT).show();
-
-                } catch (ApiException e) {
-
-                    switch (e.getStatusCode()) {
-                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
-
-                            try {
-                                ResolvableApiException resolvableApiException = (ResolvableApiException)e;
-                                resolvableApiException.startResolutionForResult(MainActivity2.this,2);
-                            } catch (IntentSender.SendIntentException ex) {
-                                ex.printStackTrace();
-                            }
-                            break;
-
-                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
-                            //Device does not have location
-                            break;
-                    }
-                }
-            }
-        });
-    }
-
-    private boolean isGPSEnabled(){
-        LocationManager locationManager = null;
-        boolean isEnabled = false;
-
-        if(locationManager == null){
-            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        }
-        isEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        return isEnabled;
-    }
-
     @Override
     public void onBackPressed(){
         super.onBackPressed();
